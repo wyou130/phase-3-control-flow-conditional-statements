b681ff243579714bdff85c4605f65a95af1c0abb
Wendy You <wyou130@gmail.com>
2022-08-28 11:41:58 -0400

    commit tree a99b1652979e6706b600dbf91780e08b0cec3fd7

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,35 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if username.downcase == "admin" && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end 
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case 
+  when temperature < 40 then "It's brisk out there!"
+  when temperature <= 65 then "It's a little chilly out there!"
+  when temperature > 85 then "It's too dang hot out there!"
+  else "It's perfect out there!"
+  end 
 end
 
 def fizzbuzz(num)
-  # your code here
+  case
+  when num % 3 == 0 && num % 5 == 0 then "FizzBuzz"
+  when num % 3 == 0 then "Fizz"
+  when num % 5 == 0 then "Buzz"
+  else num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  if operation == "+" || "-" || "*" || "/"
+    num1.method(operation).(num2)
+  else 
+    puts "Invalid operation!"
+  end
 end
-
